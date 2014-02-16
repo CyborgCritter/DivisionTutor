7166356589cf0637a9b5944417619b5f762fd86a
W1ntur <W1ntur@github.com>
2014-02-16 03:43:35 -0700

    Header class created
    Created a class to make my header more mobile.  Modified the header to
    make changes to it easier.

---
+++ DivisionTutor/DivisionTutor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisionTutor
+{
+    class Program
+    {
+        static TrimmerHeader myHeader = new TrimmerHeader();
+
+        static void Main(string[] args)
+        {
+            // Initialize any object variables I want to change.  The assignment name must always be initialized.
+            myHeader.StudentAssignment = "Division Tutor";
+
+            // Print header before printing anything else.
+            myHeader.ClearAndPrintHeader();
+
+            // Pause the consol so the user can read the output before it closes.
+            Console.Write("\n\nPress any key to continue... ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ DivisionTutor/DivisionTutor/TrimmerHeader.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisionTutor
+{
+    class TrimmerHeader
+    {
+        //Class to make reuse of my header simpler.
+
+        private char borderCharacter = '#';
+
+        public char BorderCharacter
+        {
+            get { return borderCharacter; }
+            set { borderCharacter = value; }
+        }
+
+
+        private string studentName = "Justin Trimmer";
+
+        public string StudentName
+        {
+            get { return studentName; }
+            set { studentName = value; }
+        }
+
+        private string studentClass = "CSC202";
+
+        public string StudentClass
+        {
+            get { return studentClass; }
+            set { studentClass = value; }
+        }
+
+        private string studentBlock = "SP14022";
+
+        public string StudentBlock
+        {
+            get { return studentBlock; }
+            set { studentBlock = value; }
+        }
+
+        private string studentAssignment = "Assignment Null";
+
+        public string StudentAssignment
+        {
+            get { return studentAssignment; }
+            set { studentAssignment = value; }
+        }
+
+        public void ClearAndPrintHeader()
+        {
+            // Method to clear the console, and print my personal header.
+
+            int totalCharacterWidth = GetBorderWidth();
+            int widthMinusSideChars = totalCharacterWidth - 4;
+
+            // Clear the console.
+            Console.Clear();
+
+            // Print my Header.
+            Console.WriteLine("\n{0}{0}", new string(borderCharacter, totalCharacterWidth/2));
+            Console.WriteLine("{0}{1}{1}{0}", new string(borderCharacter, 2), new string(' ', widthMinusSideChars/2));
+
+            PrintCurrentLine(studentName, widthMinusSideChars);
+            PrintCurrentLine(studentClass, widthMinusSideChars);
+            PrintCurrentLine(studentBlock, widthMinusSideChars);
+            PrintCurrentLine(studentAssignment, widthMinusSideChars);
+
+            Console.WriteLine("{0}{1}{1}{0}", new string(borderCharacter, 2), new string(' ', widthMinusSideChars/2));
+            Console.WriteLine("{0}{0}\n\n", new string(borderCharacter, totalCharacterWidth / 2));
+        }
+
+        private int GetBorderWidth()
+        {
+            // Method to make sure the border is always wider than the longest string in the header.
+
+            int nameWidth = studentName.Length;
+            int classWidth = studentClass.Length;
+            int blockWidth = studentBlock.Length;
+            int assignmentWidth = studentAssignment.Length;
+            int longestWord = nameWidth;
+            int borderWidth = 0;
+
+            if (longestWord < classWidth)
+            {
+                longestWord = classWidth;
+            }
+            else if (longestWord < blockWidth)
+            {
+                longestWord = blockWidth;
+            }
+            else if (longestWord < assignmentWidth)
+            {
+                longestWord = assignmentWidth;
+            }
+
+            borderWidth = (longestWord % 2 == 0) ? (longestWord + 14) : (longestWord + 13);
+
+            return borderWidth;
+        }
+
+        private void PrintCurrentLine(string lineText, int widthMinusSideChars)
+        {
+            // Method to calculate the number of spaces on each side of the current word between the border.
+
+            int widthOfWord = lineText.Length;
+            int spacesToDivide = widthMinusSideChars - widthOfWord;
+            // If the length of the word passed in is odd, add one extra space before the word to account for this.
+            int numberOfSpacesInFront = (spacesToDivide % 2 == 0) ? (spacesToDivide / 2) : ((spacesToDivide / 2) + 1);
+            int numberOfSpacesInBack = spacesToDivide / 2;
+            
+            Console.WriteLine("{0}{1}{2}{3}{0}", new string(borderCharacter, 2), new string(' ', numberOfSpacesInFront), lineText, new string(' ', numberOfSpacesInBack));
+        }
+    }
+}
